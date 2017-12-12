e64e5c0579d2b7f59b84fd29e443c3417635fd41
Unknown <somluk2792@gmail.com>
2017-12-12 11:57:02 +0700

    แก้ไข 5_4

--- lab_5_4.cpp
+++ lab_5_4.cpp
@@ -11,10 +11,10 @@ int main() {
 
     switch (menu) {
         case 1: cout << "Call Program 1" << endl; break;
-        case 2: cout << "Call Program 1" << endl; break;
-        case 3: cout << "Call Program 1" << endl; break;
-        case 4: cout << "Call Program 1" << endl; break;
-        case 5: cout << "Call Program 1" << endl; break;
+        case 2: cout << "Call Program 2" << endl; break;
+        case 3: cout << "Call Program 3" << endl; break;
+        case 4: cout << "Call Program 4" << endl; break;
+        case 5: cout << "Call Program 5" << endl; break;
         default: cout << "ERROR" << endl;
     }
 
